41787f2c59a1b66e31cae70e2d75e5f74ba8c73f
nnna1224 <nagidec.24a@gmail.com>
2026-03-04 17:20:07 +0900

    update entrypoint etc

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+set -e
+
 git config --global user.name "${GIT_USER_NAME}"
 git config --global user.email "${GIT_USER_EMAIL}"
 git config --global --add safe.directory /app
 
-yarn install
+if [ ! -d "node_modules" ]; then
+    yarn install
+fi
+
 yarn start --host 0.0.0.0 --no-open &
 exec code-server --bind-addr 0.0.0.0:8080 --auth password .
